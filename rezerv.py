690853f9df272b8749c10051ef87dd6a90170c26
sslypushenko <sslypushenko@mirantis.com>
2015-09-01 16:54:46 +0300

    Add verbosity

--- rezerv.py
+++ rezerv.py
@@ -4,6 +4,8 @@ import argparse
 import requests
 import json
 
+VTYPE_SHORT = {'l': '"Л"', 'k': '"К"', 'p': '"П"'}
+VTYPE_LONG = {'l': 'ЛЮКС', 'k': 'КУПЕ', 'p': 'ПЛАЦ', 'o': 'ОБЩЙ', 'c': 'СИДЧ'}
 
 def parse_cli_args(args=None):
 
@@ -19,13 +21,16 @@ def parse_cli_args(args=None):
 
     parser_trains = subparsers.add_parser('trains', parents=[],
                                           help='Look for trains')
-
+    parser_trains.add_argument('-v', '--verbose',
+                               action='count',
+                               help='Show verbose output.')
     parser_trains.add_argument('-f', dest='from_city', type=int,
                                help='Code city from')
     parser_trains.add_argument('-t', dest='to_city', type=int,
                                help='Code city to')
     parser_trains.add_argument('date', type=str,
                                help='Travel date')
+
     parser_trains.set_defaults(func='trains')
 
     parser_stations = subparsers.add_parser('station', parents=[],
@@ -38,11 +43,32 @@ def parse_cli_args(args=None):
 
 def trains(args):
 
-    url = 'http://www.pz.gov.ua/rezervGR/aj_g60.php'
+    url = 'http://www.pz.gov.ua/'
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+                         'AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Ubuntu Chromium/41.0.2272.76 '
+                         'Chrome/41.0.2272.76 Safari/537.36'}
+    resp = requests.get(url, headers=headers)
+    session_id = resp.cookies['PHPSESSID']
+    url = 'http://www.pz.gov.ua/rezervGR/aj_g60.php'
+    headers = {'Accept': 'application/json, text/javascript, */*',
+               'Accept-Encoding': 'gzip, deflate',
+               'Accept-Language': 'ru,en-US;q=0.8,en;q=0.6,uk;q=0.4',
+               'Cache-Control': 'no-cache',
+               'Connection': 'keep-alive',
+               'Content-Length': '105',
+               'Content-Type': 'application/x-www-form-urlencoded',
+               'Cookie': 'PHPSESSID=%s' % session_id,
+               'DNT': '1',
+               'Host': 'www.pz.gov.ua',
+               'Origin': 'http://www.pz.gov.ua',
+               'Pragma': 'no-cache',
+               'Referer': 'http://www.pz.gov.ua/rezervGR/',
+               'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
                              'AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Ubuntu Chromium/41.0.2272.76 '
-                             'Chrome/41.0.2272.76 Safari/537.36'}
+                             'Chrome/41.0.2272.76 Safari/537.36',
+               'X-Requested-With': 'XMLHttpRequest'}
     data = {'kstotpr': args.from_city,
             'kstprib': args.to_city,
             'sdate': args.date}
@@ -53,16 +79,52 @@ def trains(args):
                      train['otpr'],train['from']['0'],
                      train['to']['0'], train['prib'],
                      train['vputi']))
-        if 'l' in train:
-            print 'ЛЮКС %s' % train['l']
-        if 'k' in train:
-            print 'КУПЕ %s' % train['k']
-        if 'p' in train:
-            print 'ПЛАЦ %s' % train['p']
-        if 'c' in train:
-            print 'СИДЧ %s' % train['c']
-        if 'o' in train:
-            print 'ОБЩЙ %s' % train['o']
+        for vtype in ('l', 'k', 'p', 'c', 'o'):
+            print '%s %s' % (VTYPE_LONG[vtype], train[vtype])
+            if args.verbose >= 1 and vtype in ('l', 'k', 'p'):
+                data = {'nomtrain': '"%s"' % train['train']['0'],
+                        'typevag': VTYPE_SHORT[vtype],
+                        'nametrain': '%s-%s' % (train['from']['0'],
+                                                train['to']['0']),
+                        'timeotpr': train['otpr'],
+                        'timeprib': train['prib']}
+                url = 'http://www.pz.gov.ua/rezervGR/aj_g81.php'
+                resp = requests.post(url, headers=headers, data=data).json()
+                stat = {'total': 0,
+                        'lowers': 0,
+                        'pairs': 0,
+                        'coupes': 0}
+                for vagon in resp.get('vagons', []):
+                    places = [int(p) for p in vagon['mesta']]
+                    if not places:
+                        continue
+                    uppers = [p for p in places if p % 2 == 0]
+                    lowers = [p for p in places if p % 2 == 1]
+                    pairs = [(p, p + 1) for p in places
+                             if p % 2 == 1
+                             and p + 1 in places]
+                    coupes = [(p, p + 1, p + 2, p + 3) for p in places
+                              if p % 4 == 1
+                              and p + 1 in places
+                              and p + 2 in places
+                              and p + 3 in places]
+                    stat['total'] += len(places)
+                    stat['lowers'] += len(lowers)
+                    stat['pairs'] += len(pairs)
+                    stat['coupes'] += len(coupes)
+                    if args.verbose >= 2:
+                        print 'ВАГОН: ', vagon['number']
+                        if uppers:
+                            print 'ВЕРХНИЕ: %s' % uppers
+                        if lowers:
+                            print 'НИЖНИЕ: %s' % lowers
+                        if pairs:
+                            print 'ВЕРХ+НИЗ: %s' % pairs
+                        if coupes:
+                            print 'КУПЕ: %s' % coupes
+                print ('ВСЕГО: %s НИЖНИЕ: %s ВЕРХ+НИЗ: %s КУПЕ: %s'
+                       '' % (stat['total'], stat['lowers'],
+                             stat['pairs'], stat['coupes']))
 
 
 def stations(args):
